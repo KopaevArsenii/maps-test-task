b823c7ae475d480f1817b718afc558d78f42a58d
Arsenii Kopaev <Senya.Kopaev@mail.ru>
2024-01-26 16:07:51 +0300

    Changed the way to find the middle of the road

--- client/src/components/RoadMap/RoadMap.tsx
+++ client/src/components/RoadMap/RoadMap.tsx
@@ -22,11 +22,51 @@ const RoadMap:FC<RoadMapProps> = ({ roads }) => {
         return coordinates.map(coordinate => swapCoordinate(coordinate))
     }
 
-
-    function calculateCenterPosition(coordinates: Coordinate[]): Coordinate {
-        const index = Math.round(coordinates.length / 2);
-        return swapCoordinate(coordinates[index]);
+    function calculateRoadLength(road: Road): string {
+        return (road.end - road.begin).toFixed(2);
     }
+
+    function distance(a: Coordinate, b: Coordinate): number {
+        const [x1, y1] = a;
+        const [x2, y2] = b;
+        return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    }
+
+    function interpolate(a: Coordinate, b: Coordinate, ratio: number): Coordinate {
+        const [x1, y1] = a;
+        const [x2, y2] = b;
+        return [x1 + (x2 - x1) * ratio, y1 + (y2 - y1) * ratio];
+    }
+
+    function calculateCenterCoordinate(points: Coordinate[]): Coordinate {
+        //edge case with 2 coordinates
+        if (points.length == 2) {
+            return interpolate(points[0], points[1], 0.5);
+        }
+
+        const totalLength = points.reduce((acc, curr, idx, arr) => {
+            if (idx === 0) return 0;
+            const prev = arr[idx - 1];
+            return acc + distance(prev, curr);
+        }, 0);
+
+        let accumulatedLength = 0;
+        const halfLength = totalLength / 2;
+
+        for (let i = 0; i < points.length - 1; i++) {
+            const d = distance(points[i], points[i + 1]);
+            if (accumulatedLength + d >= halfLength) {
+                const overshoot = halfLength - accumulatedLength;
+                const ratio = overshoot / d;
+                return interpolate(points[i], points[i + 1], ratio)
+            }
+            accumulatedLength += d;
+        }
+        // Return the last point if something goes wrong
+        return points[points.length - 1];
+    }
+
+
 
     return (
         <div className="map_wrapper">
@@ -38,7 +78,7 @@ const RoadMap:FC<RoadMapProps> = ({ roads }) => {
                 {roads.map(road => {
                     return (
                         <Polyline key={road.id} positions={swapCoordinatesArray(road.coordinates[0])} color="red">
-                            <Marker position={calculateCenterPosition(road.coordinates[0])}>
+                            <Marker position={swapCoordinate(calculateCenterCoordinate(road.coordinates[0]))}>
                                 <Popup>
                                     <div className={"info-wrapper"}>
                                         <InfoBlock title={"Id:"} info={road.id} />
@@ -46,6 +86,7 @@ const RoadMap:FC<RoadMapProps> = ({ roads }) => {
                                         <InfoBlock title={"Номер:"} info={road.number} />
                                         <InfoBlock title={"Тип:"} info={road.type} />
                                         <InfoBlock title={"Уникальный номер:"} info={road.unique_value} />
+                                        <InfoBlock title={"Длинна дороги:"} info={calculateRoadLength(road)} />
                                     </div>
                                 </Popup>
                             </Marker>
